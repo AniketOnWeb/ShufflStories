8316a184af4bfd8c8b355badcfc1733a4729c934
Aniket Chaudhary <chaudharyaniket16@gmail.com>
2020-11-11 16:47:07 +0530

    home popper

--- src/Components/Sections/Footer.js
+++ src/Components/Sections/Footer.js
@@ -1,7 +1,17 @@
-import { Box, makeStyles, useTheme, Typography } from "@material-ui/core";
+import {
+  Box,
+  makeStyles,
+  useTheme,
+  Typography,
+  Popper,
+  Fade,
+  fade,
+} from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import CustomSvg from "../../Common/CustomSvg";
+import houseJson from "../../Common/house.json";
+import Lottie from "react-lottie";
 
 const useStyles = makeStyles((theme) => ({
   regularHigh12: theme.regularHigh12,
@@ -20,13 +30,40 @@ const useStyles = makeStyles((theme) => ({
     color: theme.colorPreset.lowEmphasisDark,
     fontWeight: "400",
   },
+  paper: {
+    // border: ".15rem solid #424141",
+    padding: "0 1.6rem",
+    backgroundColor: "#393838",
+    borderRadius: ".4rem",
+    transform: "translateY(-1rem)",
+  },
 }));
 const Footer = () => {
   const theme = useTheme();
   const classes = useStyles(theme);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "transitions-popper" : undefined;
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: houseJson,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Box padding="0 15.5rem" className={classes.heroWrapper}>
-      <Box>
+      <Box
+        aria-describedby={id}
+        onMouseEnter={handleClick}
+        onMouseLeave={() => setAnchorEl(null)}
+      >
         <Typography className={classes.normalText}>Home</Typography>
       </Box>
       <Box display="flex" flexDirection="row" alignItems="center">
@@ -44,6 +81,40 @@ const Footer = () => {
           © shufflstories • 2020
         </Typography>
       </Box>
+      <Popper id={id} open={open} anchorEl={anchorEl} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Box>
+              <Box
+                style={{
+                  left: "50%",
+                  bottom: "-.6rem",
+                  transform: "translateX(-50%)",
+                  position: "absolute",
+                  zIndex: -1,
+                }}
+              >
+                <CustomSvg type="polygon" width="2rem" height="2rem" />
+              </Box>
+              <div className={classes.paper}>
+                <Box display="flex" flexDirection="row" alignItems="center">
+                  <Typography
+                    className={classes.normalText}
+                    style={{ color: fade("#A5A4A4", 0.9) }}
+                  >
+                    You are home
+                  </Typography>
+                  <Lottie
+                    options={defaultOptions}
+                    height="6rem"
+                    width="3.5rem"
+                  />
+                </Box>
+              </div>
+            </Box>
+          </Fade>
+        )}
+      </Popper>
     </Box>
   );
 };
